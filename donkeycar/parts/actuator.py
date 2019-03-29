d19811d0dfe997965ef96ad09ed6ffd0f251e8a3
DGarbanzo <dirk.prange@web.de>
2019-03-29 23:17:00 +0000

    First implementation for RC PWM reader part using pigpio template
    * Copied code from http://abyz.me.uk/rpi/pigpio/code/read_PWM_py.zip
    * Small factoring out of common function and adapted to donkey part

--- donkeycar/parts/actuator.py
+++ donkeycar/parts/actuator.py
@@ -32,7 +32,7 @@ class PCA9685:
 
 class PWMSteering:
     """
-    Wrapper over a PWM motor cotnroller to convert angles to PWM pulses.
+    Wrapper over a PWM motor controller to convert angles to PWM pulses.
     """
     LEFT_ANGLE = -1
     RIGHT_ANGLE = 1
@@ -137,3 +137,110 @@ class Adafruit_DCMotor_Hat:
 
     def shutdown(self):
         self.mh.getMotor(self.motor_num).run(Adafruit_MotorHAT.RELEASE)
+
+import pigpio
+
+class RCReceiver:
+    """
+    Class to read PWM from an RC control. Uses pigpio library.
+    This is essentially a copy of (credit)
+    http://abyz.me.uk/rpi/pigpio/code/read_PWM_py.zip
+
+    A class to read PWM pulses and calculate their frequency
+    and duty cycle.  The frequency is how often the pulse
+    happens per second.  The duty cycle is the percentage of
+    pulse high time per cycle.
+    """
+
+    def __init__(self, pi, gpio, weighting=0.0):
+        """
+        Instantiate with the Pi and gpio of the PWM signal
+        to monitor.
+
+        Optionally a weighting may be specified.  This is a number
+        between 0 and 1 and indicates how much the old reading
+        affects the new reading.  It defaults to 0 which means
+        the old reading has no effect.  This may be used to
+        smooth the data.
+        """
+        self.pi = pi
+        self.gpio = gpio
+
+        if weighting < 0.0:
+            weighting = 0.0
+        elif weighting > 0.99:
+            weighting = 0.99
+
+        self._new = 1.0 - weighting  # Weighting for new reading.
+        self._old = weighting  # Weighting for old reading.
+        self._high_tick = None
+        self._period = None
+        self._high = None
+
+        pi.set_mode(self.gpio, pigpio.INPUT)
+        self._cb = pi.callback(self.gpio, pigpio.EITHER_EDGE, self._cbf)
+
+    def _update_param(self, param, tick):
+        """ Helper function for callback function _cbf"""
+        if self._high_tick is not None:
+            t = pigpio.tickDiff(self._high_tick, tick)
+            if param is not None:
+                return self._old * param + self._new * t
+            else:
+                return t
+
+
+    def _cbf(self, gpio, level, tick):
+        """ Callback function """
+        if level == 1:
+            self._period = _update_param(self._period, tick)
+            self._high_tick = tick
+        elif level == 0:
+            self._high = _update_param(self._high, tick)
+
+
+    def frequency(self):
+        """
+        Returns the PWM frequency.
+        """
+        if self._period is not None:
+            return 1000000.0 / self._period
+        else:
+            return 0.0
+
+    def pulse_width(self):
+        """
+        Returns the PWM pulse width in microseconds.
+        """
+        if self._high is not None:
+            return self._high
+        else:
+            return 0.0
+
+    def duty_cycle(self):
+        """
+        Returns the PWM duty cycle percentage.
+        """
+        if self._high is not None:
+            return 100.0 * self._high / self._period
+        else:
+            return 0.0
+
+    def cancel(self):
+        """
+        Cancels the reader and releases resources.
+        """
+        self._cb.cancel()
+
+    def run(self):
+        """
+        Donkey parts interface, returns pulse width in micro sec
+        """
+        return self.pulse_width()
+
+    def shutdown(self):
+        """
+        Donkey parts interface
+        """
+        self.cancel()
+        
