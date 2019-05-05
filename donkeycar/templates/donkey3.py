85c380ba706ba11ed852ac1b80eada980cb8b831
DGarbanzo <dirk.prange@web.de>
2019-05-05 22:13:52 +0100

    Resurrect 'drive' in new manage.py script and add 'calibrate' to facilitate calibration of the RC controllers. Rename variable according to pep8.

--- donkeycar/templates/donkey3.py
+++ donkeycar/templates/donkey3.py
@@ -3,7 +3,8 @@
 Script to drive a donkey 2 car using the RC controller instead of the web controller
 
 Usage:
-    manage.py
+    manage.py (drive)
+    manage.py (calibrate)
 
 Options:
     -h --help        Show this screen.
@@ -16,36 +17,36 @@ from donkeycar.parts.camera import PiCamera
 from donkeycar.parts.actuator import PCA9685, PWMSteering, PWMThrottle, RCReceiver
 from donkeycar.parts.datastore import TubGroup, TubWriter, TubHandler
 from donkeycar.parts.clock import Timestamp
+from donkeycar.parts.transform import Lambda
 import pigpio
-
 
 
 def drive(cfg):
 
     """
-    Construct a working robotic vehicle from many parts.
-    Each part runs as a job in the Vehicle loop, calling either
-    it's run or run_threaded method depending on the constructor flag `threaded`.
-    All parts are updated one after another at the framerate given in
-    cfg.DRIVE_LOOP_HZ assuming each part finishes processing in a timely manner.
-    Parts may have named outputs and inputs. The framework handles passing named outputs
-    to parts requesting the same named input.
+    Construct a working robotic vehicle from many parts. Each part runs as a job
+    in the Vehicle loop, calling either its run or run_threaded method depending
+    on the constructor flag `threaded`. All parts are updated one after another
+    at the framerate given in cfg.DRIVE_LOOP_HZ assuming each part finishes
+    processing in a timely manner. Parts may have named outputs and inputs. The
+    framework handles passing named outputs to parts requesting the same named
+    input.
     """
 
-    V = dk.vehicle.Vehicle()
+    donkey_car = dk.vehicle.Vehicle()
 
     clock = Timestamp()
-    V.add(clock, outputs=['timestamp'])
+    donkey_car.add(clock, outputs=['timestamp'])
 
     cam = PiCamera(resolution=cfg.CAMERA_RESOLUTION)
-    V.add(cam, outputs=['cam/image_array'], threaded=True)
+    donkey_car.add(cam, outputs=['cam/image_array'], threaded=True)
 
-    # Create the RC receiver
+    # create the RC receiver
     pi = pigpio.pi()
     rc_steering = RCReceiver(pi, cfg.STEERING_RC_GPIO, invert=True)
     rc_throttle = RCReceiver(pi, cfg.THROTTLE_RC_GPIO)
-    V.add(rc_steering, outputs=['user/angle', 'user/steering_on'])
-    V.add(rc_throttle, outputs=['user/throttle', 'user/throttle_on'])
+    donkey_car.add(rc_steering, outputs=['user/angle', 'user/steering_on'])
+    donkey_car.add(rc_throttle, outputs=['user/throttle', 'user/throttle_on'])
 
     steering_controller = PCA9685(cfg.STEERING_CHANNEL)
     steering = PWMSteering(controller=steering_controller,
@@ -58,8 +59,8 @@ def drive(cfg):
                            zero_pulse=cfg.THROTTLE_STOPPED_PWM,
                            min_pulse=cfg.THROTTLE_REVERSE_PWM)
 
-    V.add(steering, inputs=['user/angle'])
-    V.add(throttle, inputs=['user/throttle'])
+    donkey_car.add(steering, inputs=['user/angle'])
+    donkey_car.add(throttle, inputs=['user/throttle'])
 
     # add tub to save data
     inputs = ['cam/image_array', 'user/angle', 'user/throttle', 'timestamp']
@@ -71,16 +72,53 @@ def drive(cfg):
 
     # single tub
     # tub = TubWriter(path=cfg.TUB_PATH, inputs=inputs, types=types)
-    V.add(tub, inputs=inputs, run_condition='user/throttle_on')
+    donkey_car.add(tub, inputs=inputs, run_condition='user/throttle_on')
 
     # run the vehicle
-    V.start(rate_hz=cfg.DRIVE_LOOP_HZ, max_loop_count=cfg.MAX_LOOPS)
+    donkey_car.start(rate_hz=cfg.DRIVE_LOOP_HZ, max_loop_count=cfg.MAX_LOOPS)
+
+
+def calibrate(cfg):
+    """
+    Construct an auxiliary robotic vehicle from only the RC controllers and
+    prints their values. The RC remote usually has a tuning pot for the throttle
+    and steering channel. In this loop we run the controllers and simply print
+    their values in order to allow centering the RC pwm signals.
+    """
+    donkey_car = dk.vehicle.Vehicle()
+
+    clock = Timestamp()
+    donkey_car.add(clock, outputs=['timestamp'])
+
+    # create the RC receiver
+    pi = pigpio.pi()
+    rc_steering = RCReceiver(pi, cfg.STEERING_RC_GPIO, invert=True)
+    rc_throttle = RCReceiver(pi, cfg.THROTTLE_RC_GPIO)
+    donkey_car.add(rc_steering, outputs=['user/angle', 'user/steering_on'])
+    donkey_car.add(rc_throttle, outputs=['user/throttle', 'user/throttle_on'])
+
+    # create the lambda function for plotting
+    def plotter(angle, steering_on, throttle, throttle_on):
+        print('angle=', angle, 'steering_on=', steering_on,
+              'throttle=', throttle, 'throttle_on=', throttle_on)
+
+    plotter_part = Lambda(plotter)
+
+    # add plotter part
+    donkey_car.add(plotter_part, inputs=['user/angle', 'user/steering_on',
+                                         'user/throttle', 'user/throttle_on'])
+
+    # run the vehicle
+    donkey_car.start(rate_hz=cfg.DRIVE_LOOP_HZ, max_loop_count=cfg.MAX_LOOPS)
 
 
 if __name__ == '__main__':
     args = docopt(__doc__)
     cfg = dk.load_config()
-    drive(cfg)
+    if args['drive']:
+        drive(cfg)
+    elif args['calibrate']:
+        calibrate(cfg)
 
 
 
