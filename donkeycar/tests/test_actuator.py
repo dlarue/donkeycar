4c5f43cc6c15d8748e233ac9541cb65f0a4120e8
DGarbanzo <dirk.prange@web.de>
2019-08-03 00:21:02 +0100

    Removed token from travis.hml, improved frange(), because mulitplications are faster than divisions, updated paremeter names to comply with pep-8.

--- donkeycar/tests/test_actuator.py
+++ donkeycar/tests/test_actuator.py
@@ -17,10 +17,10 @@ def test_PWMSteering():
 
 
 def test_map_frange():
-    p1 = (dict(x=4, X_min=2, X_max=8, Y_min=-9, Y_max=3), -5)
-    p2 = (dict(x=3.16, X_min=3., X_max=4., Y_min=1000., Y_max=2000.), 1160.0)
-    p3 = (dict(x=2.1, X_min=0.7, X_max=3.5, Y_min=-20., Y_max=20.), 0.0)
-    p4 = (dict(x=14.4, X_min=14.4, X_max=17.7, Y_min=-2., Y_max=0.), -2.0)
-    p5 = (dict(x=-1.1, X_min=-2.2, X_max=-1.1, Y_min=33.0, Y_max=44.0), 44.0)
+    p1 = (dict(x=4, x_min=2, x_max=8, y_min=-9, y_max=3), -5)
+    p2 = (dict(x=3.16, x_min=3., x_max=4., y_min=1000., y_max=2000.), 1160.0)
+    p3 = (dict(x=2.1, x_min=0.7, x_max=3.5, y_min=-20., y_max=20.), 0.0)
+    p4 = (dict(x=14.4, x_min=14.4, x_max=17.7, y_min=-2., y_max=0.), -2.0)
+    p5 = (dict(x=-1.1, x_min=-2.2, x_max=-1.1, y_min=33.0, y_max=44.0), 44.0)
     for p in (p1, p2, p3, p4, p5):
         assert map_frange(**p[0]) == approx(p[1])
